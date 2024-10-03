381f5d33195a7885265578132b5cf529936d777f
aniska123 <nayakaniska918@gmail.com>
2024-10-04 00:29:40 +0530

    setup

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_Requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    requirements=[]
+    Requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        Requirements=file_obj.readlines()
+        Requirements=[req.replace("\n","") for req in Requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPEN_E_DOT in Requirements:
+            Requirements.remove(HYPEN_E_DOT)
     
-    return requirements
+    return Requirements
 
 setup(
-name='mlproject',
+name='AirOptima',
 version='0.0.1',
 author='Aniska',
 author_email='nayakaniska918@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=get_Requirements('Requirements.txt')
 
 )
